9b947a6357b262494d9003900cbf8eae7487c8d2
arllain <arllain@gmail.com>
2018-06-28 01:43:25 -0300

    Infinity scroll

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -9,8 +9,8 @@ export class ProdutoService{
     constructor(public http : HttpClient){
     }
 
-    findByCategoria(categoria_id : string){
-        return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    findByCategoria(categoria_id : string, page: number = 0, linesPerPage: number = 24){
+        return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}&page=${page}&linesPerPage=${linesPerPage}`);
     }
 
     getSmallImageFromBucket(id : string) : Observable<any>{
@@ -26,5 +26,4 @@ export class ProdutoService{
         let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`;
         return this.http.get(url,{responseType : 'blob'});
     }
-
 }
