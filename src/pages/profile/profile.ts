bf5cb07912bf6b793aaa41a2201ef7480cc1e0d8
arllain <arllain@gmail.com>
2018-06-28 03:40:52 -0300

    Fazendo updload da foto do usuario

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -13,19 +13,23 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 })
 export class ProfilePage {
 
-  cliente : ClienteDTO;
-  picture : string;
+  cliente: ClienteDTO;
+  picture: string;
   cameraOn: boolean = false;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService : ClienteService,
-    public camera : Camera) {
+    public clienteService: ClienteService,
+    public camera: Camera) {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -33,25 +37,25 @@ export class ProfilePage {
           this.cliente = response as ClienteDTO;
           this.getImageIfExists();
         },
-        error => {
-          if (error.status == 403){
-            this.navCtrl.setRoot('HomePage');
-          }
-        });
-    }else{
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
+    } else {
       this.navCtrl.setRoot('HomePage');
     }
   }
 
   getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
-    .subscribe(response => {
-      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
-    },
-    error => {});
+      .subscribe(response => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      },
+        error => { });
   }
 
-  getCameraPicture(){
+  getCameraPicture() {
     this.cameraOn = true;
     const options: CameraOptions = {
       quality: 100,
@@ -63,7 +67,19 @@ export class ProfilePage {
     this.camera.getPicture(options).then((imageData) => {
       this.picture = 'data:image/jpeg;base64,' + imageData;
       this.cameraOn = false;
-     }, (err) => {
-     });    
+    }, (err) => {
+    });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      }, error => { });
+  }
+
+  cancel() {
+    this.picture = null;
   }
 }
