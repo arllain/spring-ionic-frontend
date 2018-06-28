84e379275f370f029821e3f4117cd80936d2a857
arllain <arllain@gmail.com>
2018-06-28 00:23:33 -0300

    Tela de confirmação de pedido

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -21,7 +21,6 @@ export class PaymentPage {
     public formBuilder: FormBuilder) {
 
       this.pedido = this.navParams.get('pedido');
-      console.log(this.pedido);
 
       this.formGroup = this.formBuilder.group({
         numeroDeParcelas: [1, Validators.required],
@@ -31,7 +30,7 @@ export class PaymentPage {
 
   nextPage() {
     this.pedido.pagamento = this.formGroup.value;
-    console.log(this.pedido);
+    this.navCtrl.setRoot('OrderConfirmationPage', {pedido: this.pedido});
   }
 
 }
