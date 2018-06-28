5e17526dcb6be908e4710e99afbfc0e3a5275c21
arllain <arllain@gmail.com>
2018-06-28 17:01:33 -0300

    modificação para envio para produção

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl : "http://localhost:8080",
+    //baseUrl : "http://localhost:8080",
+    baseUrl : "https://backend-arllain.herokuapp.com/",
     bucketBaseUrl : "https://s3-sa-east-1.amazonaws.com/arllain-spring-ionic"
 }
